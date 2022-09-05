a03bd7a82835c90e4bccf6bc3ad91cc2bb37c6cc
Stanley Wintergreen <kepocnhh@gmail.com>
2022-09-05 20:08:39 +0300

    fix pr snapshot | vcs release

--- kotlin/lib/bash/workflow/pr/snapshot/vcs/release.sh
+++ kotlin/lib/bash/workflow/pr/snapshot/vcs/release.sh
@@ -2,9 +2,10 @@
 
 echo "Workflow pull request snapshot VCS release..."
 
-. ex/util/require REPOSITORY_OWNER REPOSITORY_NAME GITHUB_RUN_NUMBER GITHUB_RUN_ID MAVEN_GROUP_ID MAVEN_ARTIFACT_ID
-
 . ex/workflow/pr/snapshot/tag.sh
+
+. ex/util/require REPOSITORY_OWNER REPOSITORY_NAME GITHUB_RUN_NUMBER GITHUB_RUN_ID TAG \
+ MAVEN_GROUP_ID MAVEN_ARTIFACT_ID
 
 GIT_COMMIT_SHA=$(ex/util/jqx -sfs assemble/vcs/commit.json .sha) \
  || . ex/util/throw $? "$(cat /tmp/jqx.o)"
@@ -29,23 +30,18 @@ BODY="$(echo "$BODY" | jq -Mc ".prerelease=true")"
 mkdir -p assemble/github
 ex/github/release.sh "$BODY" || exit 16
 
+ex/project/sign/artifact/verify.sh "$TAG" || exit 21
+
 ARTIFACTS="[]"
-ARTIFACT_NAME="${REPOSITORY_NAME}-${TAG}.jar"
-ARTIFACT="$(echo "{}" | jq -Mc ".name=\"$ARTIFACT_NAME\"")"
-ARTIFACT="$(echo "$ARTIFACT" | jq -Mc ".label=\"$ARTIFACT_NAME\"")"
-ARTIFACT="$(echo "$ARTIFACT" | jq -Mc ".path=\"assemble/project/artifact/$ARTIFACT_NAME\"")"
-ARTIFACTS="$(echo "$ARTIFACTS" | jq -Mc ".+=[$ARTIFACT]")"
-ARTIFACT_NAME="${REPOSITORY_NAME}-${TAG}-sources.jar"
-ARTIFACT="$(echo "{}" | jq -Mc ".name=\"$ARTIFACT_NAME\"")"
-ARTIFACT="$(echo "$ARTIFACT" | jq -Mc ".label=\"$ARTIFACT_NAME\"")"
-ARTIFACT="$(echo "$ARTIFACT" | jq -Mc ".path=\"assemble/project/artifact/$ARTIFACT_NAME\"")"
-ARTIFACTS="$(echo "$ARTIFACTS" | jq -Mc ".+=[$ARTIFACT]")"
-ARTIFACT_NAME="${REPOSITORY_NAME}-${TAG}.pom"
-ARTIFACT="$(echo "{}" | jq -Mc ".name=\"$ARTIFACT_NAME\"")"
-ARTIFACT="$(echo "$ARTIFACT" | jq -Mc ".label=\"$ARTIFACT_NAME\"")"
-ARTIFACT="$(echo "$ARTIFACT" | jq -Mc ".path=\"assemble/project/artifact/$ARTIFACT_NAME\"")"
-ARTIFACTS="$(echo "$ARTIFACTS" | jq -Mc ".+=[$ARTIFACT]")"
+for it in \
+ "${REPOSITORY_NAME}-${TAG}.jar" \
+ "${REPOSITORY_NAME}-${TAG}.jar.sig" \
+ "${REPOSITORY_NAME}-${TAG}-sources.jar" \
+ "${REPOSITORY_NAME}-${TAG}.pom"; do
+ ARTIFACT="$(echo "{}" | jq -Mc ".name=\"$it\"")"
+ ARTIFACT="$(echo "$ARTIFACT" | jq -Mc ".label=\"$it\"")"
+ ARTIFACT="$(echo "$ARTIFACT" | jq -Mc ".path=\"assemble/project/artifact/$it\"")"
+ ARTIFACTS="$(echo "$ARTIFACTS" | jq -Mc ".+=[$ARTIFACT]")"
+done
 
 ex/github/release/upload/artifact.sh "$ARTIFACTS" || exit 17
-
-exit 0
