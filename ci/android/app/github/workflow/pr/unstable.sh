6e17e9eb5abfa49c364e85803dcd43f29af82181
Stanley Wintergreen <kepocnhh@gmail.com>
2022-09-23 16:32:20 +0300

    workflow pr merge

--- ci/android/app/github/workflow/pr/unstable.sh
+++ ci/android/app/github/workflow/pr/unstable.sh
@@ -3,7 +3,9 @@
 echo "Workflow pr unstable start..."
 
 ci/workflow/pr/unstable/assemble/vcs.sh || exit 11
-ex/vcs/pr/merge.sh || exit 21
+ci/workflow/pr/merge.sh || exit 21
+
+exit 1 # todo
 
 ci/workflow/pr/unstable/assemble/project/prepare.sh || exit 12
 ci/workflow/pr/unstable/check.sh || exit 13
