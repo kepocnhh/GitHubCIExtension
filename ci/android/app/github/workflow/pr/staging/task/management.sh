8f5dadac8b08b89d3f07fdbf827dd44ae5759c6a
Stanley Wintergreen <kepocnhh@gmail.com>
2022-09-27 12:00:17 +0300

    fix task management

--- ci/android/app/github/workflow/pr/staging/task/management.sh
+++ ci/android/app/github/workflow/pr/staging/task/management.sh
@@ -4,32 +4,29 @@ echo "Workflow pull request staging task management..."
 
 mkdir -p assemble/github
 
-. ex/util/require GITHUB_RUN_NUMBER GITHUB_RUN_ID PR_NUMBER \
- LABEL_ID_SNAPSHOT LABEL_ID_STAGING
+. ex/util/require PR_NUMBER LABEL_ID_STAGING
 
-GIT_COMMIT_DST=$(ex/util/jqx -sfs assemble/vcs/pr${PR_NUMBER}.json .base.sha) \
- || . ex/util/throw $? "$(cat /tmp/jqx.o)"
-GIT_COMMIT_SRC=$(ex/util/jqx -sfs assemble/vcs/pr${PR_NUMBER}.json .head.sha) \
- || . ex/util/throw $? "$(cat /tmp/jqx.o)"
+. ex/util/jq/write GIT_COMMIT_DST -sfs assemble/vcs/pr${PR_NUMBER}.json .base.sha
+. ex/util/jq/write GIT_COMMIT_SRC -sfs assemble/vcs/pr${PR_NUMBER}.json .head.sha
 
 ex/github/commit/compare.sh "$GIT_COMMIT_DST" "$GIT_COMMIT_SRC" \
  || . ex/util/throw 11 "Illegal state!"
 
 FILE="assemble/github/commit_compare_${GIT_COMMIT_DST::7}_${GIT_COMMIT_SRC::7}.json"
 
-SIZE=$(jq -e ".commits|length" "$FILE") || exit 1 # todo
+SIZE=$(jq -e ".commits|length" "$FILE") \
+ || . ex/util/throw 12 "Illegal state!"
 REGEX="(^|\s)fix iss/\K[^\W|$]+"
 ISSUES=()
 for ((i=0; i<SIZE; i++)); do
- it=$(ex/util/jqx -sfs "$FILE" ".commits[$i].commit.message") \
-  || . ex/util/throw $? "$(cat /tmp/jqx.o)"
- ISSUES+=($(echo "$it" | grep -Po "$REGEX" | grep -Po "\d+"))
+ . ex/util/jq/write COMMIT_MESSAGE -sfs "$FILE" ".commits[$i].commit.message"
+ ISSUES+=($(echo "$COMMIT_MESSAGE" | grep -Po "$REGEX" | grep -Po "\d+"))
 done
 
 . ci/workflow/pr/staging/tag.sh
 
 ex/github/labels.sh \
- || . ex/util/throw 12 "Illegal state!"
+ || . ex/util/throw 13 "Illegal state!"
 SIZE=${#ISSUES[*]}
 echo "[]" > assemble/github/fixed.json
 ISSUES=($(printf "%s\n" "${ISSUES[@]}" | sort -u))
@@ -38,13 +35,10 @@ LABEL_ID_TARGET="$LABEL_ID_STAGING"
 LABEL_TARGET="$(jq ".[]|select(.id==$LABEL_ID_TARGET)" assemble/github/labels.json)"
 LABEL_NAME_TARGET="$(echo "$LABEL_TARGET" | jq -r .name)"
 
-REPOSITORY_HTML_URL=$(ex/util/jqx -sfs assemble/vcs/repository.json .html_url) \
- || . ex/util/throw $? "$(cat /tmp/jqx.o)"
+. ex/util/jq/write REPOSITORY_HTML_URL -sfs assemble/vcs/repository.json .html_url
 
-CI_BUILD_NUMBER=$(ex/util/jqx -si assemble/vcs/actions/run.json .run_number) \
- || . ex/util/throw $? "$(cat /tmp/jqx.o)"
-CI_BUILD_HTML_URL=$(ex/util/jqx -sfs assemble/vcs/actions/run.json .html_url) \
- || . ex/util/throw $? "$(cat /tmp/jqx.o)"
+. ex/util/jq/write CI_BUILD_NUMBER -si assemble/vcs/actions/run.json .run_number
+. ex/util/jq/write CI_BUILD_HTML_URL -sfs assemble/vcs/actions/run.json .html_url
 
 TAG_URL="$REPOSITORY_HTML_URL/releases/tag/$TAG"
 MESSAGE="Marked as \`$LABEL_NAME_TARGET\` in [$TAG]($TAG_URL) by CI build [#$CI_BUILD_NUMBER]($CI_BUILD_HTML_URL)."
