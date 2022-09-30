2d443ccda2b5d4e4030bd1cc83b14f75e0ea4d69
Stanley Wintergreen <kepocnhh@gmail.com>
2022-09-30 21:23:04 +0300

    fix github tag test

--- ex/github/tag/test.sh
+++ ex/github/tag/test.sh
@@ -6,42 +6,37 @@ echo "GitHub tag test..."
 
 TAG="$1"
 
-REPOSITORY_URL=$(ex/util/jqx -sfs assemble/vcs/repository.json .url) \
- || . ex/util/throw $? "$(cat /tmp/jqx.o)"
+. ex/util/json -f assemble/vcs/repository.json \
+ -sfs .url REPOSITORY_URL \
+ -sfs .html_url REPOSITORY_HTML_URL
 
 . ex/util/require VCS_DOMAIN REPOSITORY_OWNER REPOSITORY_NAME TAG
 
 CODE=0
 CODE=$(curl -s -w %{http_code} -o /tmp/tag.json "$REPOSITORY_URL/git/refs/tags/$TAG")
 case $CODE in
- 404)
-  REPOSITORY_HTML_URL=$(ex/util/jqx -sfs assemble/vcs/repository.json .html_url) \
-   || . ex/util/throw $? "$(cat /tmp/jqx.o)"
-  echo "The tag \"$TAG\" does not exist yet in ${REPOSITORY_HTML_URL}."
-  exit 0;;
+ 404) . ex/util/success "The tag \"$TAG\" does not exist yet in ${REPOSITORY_HTML_URL}.";;
  200) true;; # ignored
  *) echo "Get tag \"$TAG\" info error!"
-  echo "Request error with response code $CODE!"
-  exit 31;;
+  . ex/util/throw 31 "Request error with response code $CODE!";;
 esac
 
-TYPE="$(jq -Mcer type /tmp/tag.json)" || exit 1 # todo
+TYPE="$(jq -Mcer type /tmp/tag.json)" \
+ || . ex/util/throw 101 "Illegal state!"
 case $TYPE in
- object) REF="$(ex/util/jqx -sfs /tmp/tag.json .ref)" || exit 1 # todo
-  if test "$REF" == "refs/tags/$TAG"; then
-   echo "The tag \"$TAG\" already exists!"; exit 41
-  fi
+ object)
+  . ex/util/json -f /tmp/tag.json -sfs .ref REF
+  [ "$REF" == "refs/tags/$TAG" ] && . ex/util/throw 41 "The tag \"$TAG\" already exists!"
   exit 42;;
- array) REFS=($(jq -Mcer ".[].ref" /tmp/tag.json)) || exit 1 # todo
+ array)
+  REFS=($(jq -Mcer ".[].ref" /tmp/tag.json)) \
+   || . ex/util/throw 102 "Illegal state!"
   SIZE=${#REFS[*]}
-  for ((i = 0; i < SIZE; i++)); do
-   REF="${ARRAY[$i]}"
-   if test "$REF" == "refs/tags/$TAG"; then
-    echo "The tag \"$TAG\" already exists!"; exit 51
-   fi
+  for ((REF_INDEX = 0; REF_INDEX < SIZE; REF_INDEX++)); do
+   REF="${ARRAY[$REF_INDEX]}"
+   [ "$REF" == "refs/tags/$TAG" ] && . ex/util/throw 51 "The tag \"$TAG\" already exists!"
   done; exit 0;;
- *) echo "The type \"$TYPE\" is not supported!"
-  exit 61;;
+ *) . ex/util/throw 61 "The type \"$TYPE\" is not supported!";;
 esac
 
-. ex/util/throw 71 "Illegal state error!"
+. ex/util/throw 71 "Illegal state!"
